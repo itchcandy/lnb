65d6b0752c2fdc81629fc2cdd1c10d066f267961
Vishwakant Mishra <vishwakant@vgminds.com>
2019-11-06 09:08:19 +0530

    Adding basic player

---
+++ Assets/Scripts/game/Direction.cs
@@ -0,0 +1,12 @@
+﻿namespace game
+{
+    public enum Direction
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
---
+++ Assets/Scripts/player/Player.cs
@@ -0,0 +1,56 @@
+﻿namespace player
+{
+    using UnityEngine;
+    using game;
+
+    public class Player : MonoBehaviour
+    {
+        public float walkSpeed = 1f;
+        public float jumpIntensity = 1f;
+        public float explosionRadius = 1f;
+        public float upwardMod = 1f;
+        Rigidbody body;
+
+        void Awake()
+        {
+            body = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.A)) Move(Direction.LEFT);
+            if(Input.GetKey(KeyCode.S)) Move(Direction.BACKWARD);
+            if(Input.GetKey(KeyCode.D)) Move(Direction.RIGHT);
+            if(Input.GetKey(KeyCode.W)) Move(Direction.FORWARD);
+            if(Input.GetKeyDown(KeyCode.Space)) Jump();
+        }
+
+        void Move(Direction direction)
+        {
+            switch(direction) {
+                case Direction.FORWARD:
+                    Walk(new Vector2(0, walkSpeed * Time.timeScale));
+                    break;
+                case Direction.BACKWARD:
+                    Walk(new Vector2(0, -1 * walkSpeed * Time.timeScale));
+                    break;
+                case Direction.LEFT:
+                    Walk(new Vector2(-1 * walkSpeed * Time.timeScale, 0));
+                    break;
+                case Direction.RIGHT:
+                    Walk(new Vector2(walkSpeed * Time.timeScale, 0));
+                    break;
+            }
+        }
+
+        void Jump()
+        {
+            body.AddExplosionForce(jumpIntensity, transform.position  + new Vector3(0, -1, 0), explosionRadius, upwardMod);
+        }
+
+        void Walk(Vector2 moveBy)
+        {
+            transform.position += new Vector3(moveBy.x, 0, moveBy.y);
+        }
+    }
+}
